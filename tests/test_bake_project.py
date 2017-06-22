c7876d37ad0aedaf14cce23ab58b61f484241204
Davide Moro <dmoro@tierratelematics.com>
2017-06-22 12:05:08 +0200

    debug travis

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -62,7 +62,7 @@ def run_inside_dir(command, dirpath):
     :param dirpath: String, path of the directory the command is being run.
     """
     with inside_dir(dirpath):
-        if os.getenv('TRAVIS'):
+        if os.getenv('TRAVIS') or True:       # DEBUG travis
             with open(os.devnull, 'w') as devnull:
                 return subprocess.check_call(shlex.split(command),
                                              stdout=devnull,
